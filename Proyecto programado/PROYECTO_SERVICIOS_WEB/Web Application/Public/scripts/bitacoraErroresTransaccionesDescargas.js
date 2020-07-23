80fb474c8ee01cfa94381911bf6b102697d5da87
Carlos Rodriguez <Carrodjim@hotmail.com>
2020-07-22 23:50:16 -0600

    Adiciones
    Creadas consultas para las bitacoras. Creadas consultas para los errores. Cambios en la tabla de Bitacora y sus Stored Procedures

--- Proyecto programado/PROYECTO_SERVICIOS_WEB/Web Application/Public/scripts/bitacoraErroresTransaccionesDescargas.js
+++ Proyecto programado/PROYECTO_SERVICIOS_WEB/Web Application/Public/scripts/bitacoraErroresTransaccionesDescargas.js
@@ -24,47 +24,173 @@ function boton_consultar() {
     }
 }
 
-function traer_bitacoras() {
-    const url = "https://localhost:44371/api/bitacora";
+// Este metodo se encarga de dar vuelta a los valores del array
+// de tal forma que los valores más nuevos son mostrados de
+// primero.
+function voltearValoresObj(objResponse) {
+    let objResponseLength = objResponse.length - 1;
+    let objNew = {};
 
-    fetch(url)
+    for (let index = 0; index < objResponse.length; index++) {
+        objNew[index] = objResponse[objResponseLength];
+        objResponseLength--;
+    }
+    // Le damos la propiedad length y le asignamos la length actual.
+    objNew.length = objResponse.length;
+    return objNew;
+}
+
+function traer_bitacoras() {
+
+    const APIUrl = "https://localhost:44371/api/bitacora";
+
+    fetch(APIUrl)
         .then(function (response) {
             return response.text();
         })
         .then(function (response) {
-            let json = JSON.parse(response);
-            //renderizar_categorias_libros(json, contenedor_id);
-            if (json != null) {
+            let objResponse = JSON.parse(response);
+            if (objResponse != null) {
 
-                let html = "";
+                let contenidoHtml = "";
 
-                html +=
-                    `<p>Total de datos: ${json.length}</p>
-                    <p>Registros: <br>
+                let objBitacoras = voltearValoresObj(objResponse);
+
+                contenidoHtml +=
+                    `<p>Total de registros: ${objBitacoras.length}</p>
+                    <p>Registros:</p> <br>
+                    <table class="table table-striped">
+                            <tr>
+                                <th scope="col">Codigo del registro</th>    
+                                <th scope="col">Admin</th>
+                                <th scope="col">Fecha y hora</th>
+                                <th scope="col">Tipo</th>
+                                <th scope="col">Descripcion</th>
+                                <th scope="col">Registro en detalle</th>
+                            </tr>
                     `;
-                for (let index = 0; index < json.length; index++) {
-                    html += json[index].id + "<br>" + json[index].categoria + "<br><br>";
-                }
-                html += "</p>";
+                for (let index = 0; index < objBitacoras.length; index++) {
 
-                document.getElementById("contenido").innerHTML = "** Cargando datos...";
-                document.getElementById("contenido").innerHTML = html;
+                    contenidoHtml += `<tr>
+                                <td>` + objBitacoras[index].id + `</td>            
+                                <td>` + objBitacoras[index].nombreUsuarioAdmin + `</td>
+                                <td>` + objBitacoras[index].fechaYHora + `</td>
+                                <td>` + objBitacoras[index].tipo + `</td>
+                                <td>` + objBitacoras[index].descripcion + `</td>
+                                <td>` + objBitacoras[index].registroEnDetalle + `</td>
+                            </tr>
+                            `
+                }
+                contenidoHtml += "</table>";
+                document.getElementById("contenido").innerHTML = contenidoHtml;
+
             } else {
                 document.getElementById("contenido").innerHTML = "No hay registros de bitacora.";
             }
         })
         .catch(function (err) {
             console.error(err);
-            document.getElementById("contenido").innerHTML = "*** Ha ocurrido un error";
+            document.getElementById("contenido").innerHTML = "*** Ha ocurrido un error ***";
         });
 }
 
 function traer_errores() {
+    const APIUrl = "https://localhost:44371/api/error";
 
+    fetch(APIUrl)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (response) {
+            let objResponse = JSON.parse(response);
+            if (objResponse != null) {
+
+                let contenidoHtml = "";
+
+                let objBitacoras = voltearValoresObj(objResponse);
+
+                contenidoHtml +=
+                    `<p>Total de registros: ${objBitacoras.length}</p>
+                    <p>Registros:</p> <br>
+                    <table class="table table-striped">
+                            <tr>
+                                <th scope="col">Num registro</th>    
+                                <th scope="col">Admin</th>
+                                <th scope="col">Fecha</th>
+                                <th scope="col">Detalles del error</th>
+                            </tr>
+                    `;
+                for (let index = 0; index < objBitacoras.length; index++) {
+
+                    contenidoHtml += `<tr>
+                                <td>` + objBitacoras[index].id + `</td>            
+                                <td>` + objBitacoras[index].idUsuario + `</td>
+                                <td>` + objBitacoras[index].fechaYHora + `</td>
+                                <td>` + objBitacoras[index].mensajeDeError + `</td>
+                            </tr>
+                            `
+                }
+                contenidoHtml += "</table>";
+                document.getElementById("contenido").innerHTML = contenidoHtml;
+
+            } else {
+                document.getElementById("contenido").innerHTML = "No hay registros de errores.";
+            }
+        })
+        .catch(function (err) {
+            console.error(err);
+            document.getElementById("contenido").innerHTML = "*** Ha ocurrido un error ***";
+        });
 }
 
 function traer_transacciones() {
+    const APIUrl = "https://localhost:44371/api/transaccion";
 
+    fetch(APIUrl)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (response) {
+            let objResponse = JSON.parse(response);
+            if (objResponse != null) {
+
+                let contenidoHtml = "";
+
+                let objBitacoras = voltearValoresObj(objResponse);
+
+                contenidoHtml +=
+                    `<p>Total de registros: ${objBitacoras.length}</p>
+                    <p>Registros:</p> <br>
+                    <table class="table table-striped">
+                            <tr>
+                                <th scope="col">Num registro</th>    
+                                <th scope="col">Fecha</th>
+                                <th scope="col">Monto</th>
+                                <th scope="col">Monto</th>
+                                <th scope="col">Detalles del error</th>
+                            </tr>
+                    `;
+                for (let index = 0; index < objBitacoras.length; index++) {
+
+                    contenidoHtml += `<tr>
+                                <td>` + objBitacoras[index].id + `</td>            
+                                <td>` + objBitacoras[index].idUsuario + `</td>
+                                <td>` + objBitacoras[index].fechaYHora + `</td>
+                                <td>` + objBitacoras[index].mensajeDeError + `</td>
+                            </tr>
+                            `
+                }
+                contenidoHtml += "</table>";
+                document.getElementById("contenido").innerHTML = contenidoHtml;
+
+            } else {
+                document.getElementById("contenido").innerHTML = "No hay registros de transacciones.";
+            }
+        })
+        .catch(function (err) {
+            console.error(err);
+            document.getElementById("contenido").innerHTML = "*** Ha ocurrido un error ***";
+        });
 }
 
 function traer_descargas() {
